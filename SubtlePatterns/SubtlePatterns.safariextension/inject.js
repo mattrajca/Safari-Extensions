f87e50cca9225738e3c082e2b779c12397f5fcbe
Don McCurdy <don.r.mccurdy@gmail.com>
2012-11-18 13:11:20 -0600

    "Added feature allowing user to specify selector to apply pattern to. Defaults to "body".

--- SubtlePatterns/SubtlePatterns.safariextension/inject.js
+++ SubtlePatterns/SubtlePatterns.safariextension/inject.js
@@ -1,6 +1,9 @@
 safari.self.addEventListener("message", function (event) {
 	if (event.name === "changeBg") {
-		document.body.style.backgroundRepeat = "repeat";
-		document.body.style.backgroundImage = event.message;
+		var elements = document.querySelectorAll(event.message['selector']);
+		for (var i = 0; i < elements.length; ++i ) {
+			elements[i].style.backgroundRepeat = "repeat";
+			elements[i].style.backgroundImage = event.message['image'];
+		}
 	}
 }, false);
